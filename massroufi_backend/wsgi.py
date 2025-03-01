d5c3cf5e2626f72563bc405ccf2e49eb96b7bf7c
Ysen0603 <yassineennaya2264@gmail.com>
2025-03-01 12:22:15 +0000

    "wsgi for vercel"

--- massroufi_backend/wsgi.py
+++ massroufi_backend/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'massroufi_backend.settings')
 
 application = get_wsgi_application()
+# Add this line for Vercel
+app = application
