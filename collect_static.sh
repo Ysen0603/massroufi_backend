83860db6e8a2c2c8a74e59cd8b27f35af5121a09
Ysen0603 <yassineennaya2264@gmail.com>
2025-03-04 15:00:42 +0000

    "fix the sh file v1.3"

--- collect_static.sh
+++ collect_static.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Install project dependencies
-python -m pip install -r requirements.txt
-
 # Create static directory if it doesn't exist
 mkdir -p static
 # Collect static files
-python manage.py collectstatic --noinput
+/opt/conda/bin/python manage.py collectstatic --noinput
